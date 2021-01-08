23f4b17efaccca3c7baded15504a778df1c90107
Arslan Aslam <72731966+arslan41000@users.noreply.github.com>
2021-01-08 17:52:31 +0500

    Initial commit

--- lib/screen/note_list_screen.dart
+++ lib/screen/note_list_screen.dart
@@ -1,32 +1,89 @@
-import 'package:flutter/cupertino.dart';
-import 'package:notes_taking_app/models/note.dart';
-import 'package:notes_taking_app/utils/constants.dart';
-import 'database_helper.dart';
-
-class NoteProvider with ChangeNotifier{
-
-  List _items = [];
-  List get items {
-    return [..._items];
-  }
-  Future addOrUpdateNote(int id, String title, String content,
-      String imagePath, EditMode editMode) async {
-    final note = Note(id, title, content, imagePath);
-    if (EditMode.ADD == editMode) {
-      _items.insert(0, note);
-    } else {
-      _items[_items.indexWhere((note) => note.id == id)] = note;
-    }
-    notifyListeners();
-    DatabaseHelper.insert(
-      {
-        'id': note.id,
-        'title': note.title,
-        'content': note.content,
-        'imagePath': note.imagePath,
-      },
+class ListItem extends StatelessWidget {
+  final int id;
+  final String title;
+  final String content;
+  final String imagePath;
+  final String date;
+  ListItem({this.id, this.title, this.content, this.imagePath, this.date});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 135.0,
+      margin: EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, NoteViewScreen.route, arguments: id);
+        },
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 12.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: shadow,
+            borderRadius: BorderRadius.circular(15.0),
+            border: Border.all(color: grey, width: 1.0),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                        style: itemTitle,
+                      ),
+                      SizedBox(height: 4.0),
+                      Text(
+                        date,
+                        overflow: TextOverflow.ellipsis,
+                        style: itemDateStyle,
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Expanded(
+                        child: Text(
+                          content,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: itemContentStyle,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              if(imagePath!=null)
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 12.0,
+                    ),
+                    Container(
+                      width: 80.0,
+                      height: 95.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.0),
+                        image: DecorationImage(
+                          image: FileImage(
+                            File(imagePath),
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+            ],
+          ),
+        ),
+      ),
     );
   }
-
-
 }
